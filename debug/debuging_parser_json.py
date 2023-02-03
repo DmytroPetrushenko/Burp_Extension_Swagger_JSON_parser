e26fea96106aa9b10ce778a10709f311cf7eb92c
dpetrushenko <dmytro.petrushenko@gmail.com>
2023-02-03 13:10:40 +0200

    add to DICT "string" -> 'uuid'

--- debug/debuging_parser_json.py
+++ debug/debuging_parser_json.py
@@ -2,7 +2,7 @@ import parser.parser_json as debug
 import requests as re
 import json
 
-json_url = 'https://crm-app-nightly.genci0.eisgroup.com/api/common/schema/v1/activities/swagger.json'
+json_url = 'https://billing-app-nightly.genci0.eisgroup.com/api/common/schema/v1/BalanceRegister/swagger.json'
 authorization = 'cWE6cWE='
 swagger_response = re.get(json_url, headers={'Authorization': 'Basic cWE6cWE='})
 json_loads = json.loads(swagger_response.text)
--- parser/constant.py
+++ parser/constant.py
@@ -8,7 +8,7 @@ HOST = 'host'
 FILE = 'file'
 DICT = {'integer': {'default': 1005000, 'int32': 0, 'int64': 1},
         'string': {'default': 'string', 'date': '2023-01-19', 'date-time': '2023-01-19T16:57:08.404Z',
-                   'password': 'password'},
+                   'password': 'password', 'uuid': 'uuid'},
         'number': {'float': 0.1, 'double': 1.1},
         'boolean': {'default': 'true'},
         'object': {'default': 'object'},
